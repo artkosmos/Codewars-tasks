34b16522d14f21e8c18d0834f6e02e1003d2d751
Artem <artemkosmatkov1994@gmail.com>
2023-02-23 17:11:13 +0300

    add new file

---
+++ convert-to-binary/solution.js
@@ -0,0 +1,5 @@
+// write a function which returns that number in a binary format.
+
+const toBinary = (n) => +n.toString(2)
+
+console.log(toBinary(11)) // 1011
---
+++ string-to-number/solution.js
@@ -0,0 +1,7 @@
+// We need a function that can transform a string into a number
+
+const stringToNumber = function (str) {
+  return +str // or pasreInt or Number()
+}
+
+console.log(typeof stringToNumber('176'))
