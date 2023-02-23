9c0464ebeb132f7339b909e2fba1e65d218f1ec2
Artem <artemkosmatkov1994@gmail.com>
2023-02-23 14:30:07 +0300

    add new file

---
+++ century-from-year/solution.js
@@ -0,0 +1,15 @@
+/* Introduction
+The first century spans from the year 1 up to and including the year 100,
+the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in. */
+
+function century (year) {
+  return Math.ceil(year / 100)
+}
+
+console.log(century(1705))
+console.log(century(1900))
+console.log(century(1601))
+console.log(century(980))
