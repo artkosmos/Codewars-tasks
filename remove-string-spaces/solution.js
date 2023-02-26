f791fa71a0d7f5b0282e9e133312e6f49677735c
Artem <artemkosmatkov1994@gmail.com>
2023-02-26 15:29:53 +0300

    add new file

---
+++ remove-string-spaces/solution.js
@@ -0,0 +1,5 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+const noSpace = (x) => x.split(' ').join('')
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'))
