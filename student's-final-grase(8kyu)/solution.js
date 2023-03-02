f2a93a98f40003db538bdab365f76d0420c1bf51
Artem <artemkosmatkov1994@gmail.com>
2023-03-02 15:53:25 +0300

    add new file

---
+++ student's-final-grase(8kyu)/solution.js
@@ -0,0 +1,11 @@
+function finalGrade (exam, projects) {
+  if (exam > 90 || projects > 10) return 100
+  if (exam > 75 && projects >= 5) return 90
+  if (exam > 50 && projects >= 2) return 75
+
+  return 0
+}
+
+console.log(finalGrade(94, 2))
+console.log(finalGrade(76, 10))
+console.log(finalGrade(42, 7))
