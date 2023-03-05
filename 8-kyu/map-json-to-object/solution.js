bb270bccf86b9e4d4e5b6ff4d417ef4690751043
Artem <artemkosmatkov1994@gmail.com>
2023-03-05 13:24:37 +0300

    add new file

---
+++ 8-kyu/map-json-to-object/solution.js
@@ -0,0 +1,23 @@
+/** ЗАДАЧА 40 - Конвертация JSON в JavaScript объекты
+ *
+ * 1. Конвертируйте массив JSON объектов в массив объектов JavaScript
+ *
+ * 2. Выведите в консоль результирующий массив
+ *
+ * 3. Выведите в консоль "postId" второго объекта
+ *
+ * 4. Выведите в консоль "commentsQuantity" последнего объекта
+ */
+
+const postsJSON = [
+  '{"postId":1355,"commentsQuantity":5}',
+  '{"postId":5131,"commentsQuantity":13}',
+  '{"postId":6134,"commentsQuantity":2}',
+  '{"postId":2351,"commentsQuantity":8}',
+]
+
+const postsObjects = postsJSON.map((element) => JSON.parse(element))
+
+console.log(postsObjects)
+console.log(postsObjects[1].postId)
+console.log(postsObjects[postsObjects.length - 1].commentsQuantity)
