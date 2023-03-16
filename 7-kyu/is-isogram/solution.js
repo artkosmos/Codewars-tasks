83f69cf05ca3d21308372bb53c2629d772dc633c
Artem <artemkosmatkov1994@gmail.com>
2023-03-16 19:29:00 +0300

    change decision

--- 7-kyu/is-isogram/solution.js
+++ 7-kyu/is-isogram/solution.js
@@ -1,14 +1,16 @@
 function isIsogram (str) {
   const newStr = str.toLowerCase()
-  return newStr.split('').every((item, index, arr) => index === arr.findIndex((element) => element === item))
+  return newStr.split('').every((item, index, arr) => index === arr.indexOf((item)))
 }
 
 console.log(isIsogram('Dermatoglyphics'))
 console.log(isIsogram('moOse'))
 console.log(isIsogram('isIsogram'))
 
-/* alternative decision through Set
+// alternative decision through Set
 
-function isIsogram(str){
-	return new Set(str.toUpperCase()).size == str.length;
-} */
+function isIsogram2 (str) {
+  return new Set(str.toUpperCase()).size === str.length;
+}
+
+console.log(isIsogram2('Dermatoglyphics'))
