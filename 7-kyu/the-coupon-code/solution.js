adbc892d1ae820a84debf94e90edf613a353f7db
Artem <artemkosmatkov1994@gmail.com>
2023-03-18 23:13:34 +0300

    add description

--- 7-kyu/the-coupon-code/solution.js
+++ 7-kyu/the-coupon-code/solution.js
@@ -1,3 +1,9 @@
+/* Your mission:
+Write a function called checkCoupon
+which verifies that a coupon code is valid and not expired.
+A coupon is no more valid on the day AFTER the expiration date.
+All dates will be passed as strings in this format: "MONTH DATE, YEAR". */
+
 function checkCoupon (enteredCode, correctCode, currentDate, expirationDate) {
   if ((enteredCode === correctCode)
     && (Date.parse(currentDate) <= Date.parse(expirationDate))) {
@@ -6,6 +12,3 @@ function checkCoupon (enteredCode, correctCode, currentDate, expirationDate) {
   return false
 }
 console.log(checkCoupon('123a', '123', 'September 5, 2014', 'October 1, 2014'))
-
-/* console.log(Date.parse('September 5, 2014'))
-console.log(Date.parse('October 1, 2014')) */
