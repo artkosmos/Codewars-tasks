e89b7f2528f0cc884434353a14c1f84e5a559419
Artem <artemkosmatkov1994@gmail.com>
2023-04-14 12:51:26 +0300

    add new decision

---
+++ 7-kyu/unpacking-arguments/solution.js
@@ -0,0 +1,9 @@
+/* You must create a function, spread, that takes a function and a list of
+arguments to be applied to that function. You must make this function
+return the result of calling the given function/lambda with the given arguments. */
+
+function spread (func, args) {
+  return func(...args)
+}
+
+console.log(spread((x, y) => x + y, [1, 2]))
