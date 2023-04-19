5eb0bbe6544e606932b91a34402333ff323432c4
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-04-19 19:21:20 +0300

    add new solution

---
+++ 8-kyu/convert-a-boolean-to-a-String/solution.js
@@ -0,0 +1,8 @@
+function booleanToString(boolean) {
+  return boolean.toString()
+}
+
+console.log(booleanToString(true))
+console.log(typeof booleanToString(true))
+console.log(booleanToString(false))
+console.log(typeof booleanToString(false))
