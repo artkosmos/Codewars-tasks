ff8e70336d8f83cedbf387a28c56da3ec5584c0d
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-04-22 18:07:54 +0300

    refactro: rename

--- 8-kyu/first_letter_always_capital/task.js
+++
@@ -1,7 +0,0 @@
-function capitalizeWord (word) {
-  return word[0].toUpperCase() + word.slice(1)
-}
-
-console.log(capitalizeWord('suck'))
-console.log(capitalizeWord('unfortunately'))
-console.log(capitalizeWord('resolve'))
