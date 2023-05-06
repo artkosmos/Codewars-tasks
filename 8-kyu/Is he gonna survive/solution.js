4b69e8d1bd777e724620a37e8d4bc70d380c1a8b
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-05-06 21:08:19 +0300

    add new solution

---
+++ 8-kyu/Is he gonna survive/solution.js
@@ -0,0 +1,16 @@
+/*
+A hero is on his way to the castle to complete his mission.
+However, he's been told that the castle is surrounded with a couple of
+powerful dragons! each dragon takes 2 bullets to be defeated,
+our hero has no idea how many bullets he should carry.
+Assuming he's going to grab a specific given number of bullets and move forward
+to fight another specific given number of dragons, will he survive?
+Return true if yes, false otherwise :)
+*/
+
+function hero(bullets, dragons){
+  return (bullets / dragons) >= 2
+}
+
+console.log(hero(10,5))
+console.log(hero(7,4))
