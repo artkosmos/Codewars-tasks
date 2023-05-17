8f498436336d02151bd510519e161771aaa4e191
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-05-17 11:58:42 +0300

    refactor: change solution

--- 7-kyu/Sum of two lowest positive integers/solution.js
+++ 7-kyu/Sum of two lowest positive integers/solution.js
@@ -3,11 +3,8 @@ Create a function that returns the sum of the two lowest positive numbers given 
 of minimum 4 positive integers. No floats or non-positive integers will be passed.*/
 
 function sumTwoSmallestNumbers(numbers) {
-  const numbersCopy = [...numbers]
-  const firstMinNum = numbersCopy.splice(numbersCopy.indexOf(Math.min(...numbersCopy)), 1)
-  const secondMinNum = numbersCopy.splice(numbersCopy.indexOf(Math.min(...numbersCopy)), 1)
-
-  return [...firstMinNum, ...secondMinNum].reduce((acc, item) => acc + item)
+  const [number1, number2] = numbers.sort((a, b) => a - b)
+  return number1 + number2
 }
 
 console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]))
