52ee846d418bda70d1ae7dff0dfe01d3f4229897
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-05-19 22:16:43 +0300

    add new solution

---
+++ 7-kyu/The highest profit wins!/solution.js
@@ -0,0 +1,8 @@
+/*
+Write a function that returns both the minimum and maximum number of the given list/array.*/
+
+function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)];
+}
+
+console.log(minMax([1]))
