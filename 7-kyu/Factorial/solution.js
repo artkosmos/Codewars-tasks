25c79a5e50b5b648069712f945a508697a621ed2
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-06-05 16:28:10 +0300

    fix previous solution

--- 7-kyu/Factorial/solution.js
+++ 7-kyu/Factorial/solution.js
@@ -2,14 +2,20 @@
 In mathematics, the factorial of a non-negative integer n, denoted by n!,
 is the product of all positive integers less than or equal to n.
 For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw RangeError
 */
 
 
 function factorial(n) {
+  if (n > 12 || n < 0) {
+    throw new RangeError('Invalid value')
+  }
   if (n < 1) {
     return 1
   }
   return n * factorial(n - 1)
 }
 
-console.log(factorial(5))
+console.log(factorial(11))
+console.log(factorial(-2))
