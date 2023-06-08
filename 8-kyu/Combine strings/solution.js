87dab6de0d154a0f3aa7ced0727c55f8e9ed6952
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-06-08 14:34:25 +0300

    add new solution

---
+++ 8-kyu/Combine strings/solution.js
@@ -0,0 +1,9 @@
+/*
+Create a function named combineNames that accepts two parameters (first and last name).
+The function should return the full name.*/
+
+const combineNames = (string1, string2) => {
+  return `${string1} ${string2}`
+}
+
+console.log(combineNames('James', 'Stevens'))
