a67e70c586b38b6d1fc4ceeaefcc63dc43a81d29
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-07-19 16:17:17 +0300

    add new solution

---
+++ 8-kyu/Find numbers which are divisible by given number/solution.js
@@ -0,0 +1,9 @@
+/*
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor.
+First argument is an array of numbers and the second is the divisor.*/
+
+function divisibleBy(numbers, divisor) {
+  return numbers.filter(item => item % divisor === 0)
+}
+
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2))
