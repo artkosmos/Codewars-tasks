adf026a74c3d5b6fb708b6f745893e41967e047b
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-08-19 16:48:44 +0300

    add new solution

---
+++ 7-kyu/Sum of odd numbers/solution.js
@@ -0,0 +1,7 @@
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
