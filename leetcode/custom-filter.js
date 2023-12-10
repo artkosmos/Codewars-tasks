00a56b0ecf93c21cdfaed4c9bde884b59a2e1ab8
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-12-10 18:24:06 +0300

    add new solution

---
+++ leetcode/custom-filter.js
@@ -0,0 +1,10 @@
+var filter = function(arr, fn) {
+  const filteredArray = []
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      filteredArray.push(arr[i])
+    }
+  }
+
+  return filteredArray
+};
